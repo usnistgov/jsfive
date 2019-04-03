d4529215ac0f34cdfc8678473aa5bd55fd5c1147
bbm <brian.maranville@nist.gov>
2019-04-03 14:58:50 -0400

    fix path to web_modules

--- esm/btree.js
+++ esm/btree.js
@@ -1,5 +1,5 @@
 import {_unpack_struct_from, _structure_size, struct, dtype_getter, DataView64} from './core.js';
-import {default as pako} from './web_modules/pako-es.js';
+import {default as pako} from '../web_modules/pako-es.js';
 
 const zlib = {
   decompress: function(buf) {
